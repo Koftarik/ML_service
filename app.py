e8d976d0ceb5d78cf3e222c7eb9b6bcbeb9f812a
Alexander Gorbuntsov <goralex02@yandex.ru>
2024-09-03 17:23:21 +0300

    Обновлено до рабочего состояния

---
+++ app.py
@@ -0,0 +1,172 @@
+import pandas as pd
+import streamlit as st
+from PIL import Image
+from model import predict_on_input, encoding_and_scaling
+import time
+
+
+def process_main_page():
+    show_main_page()
+    render_main_page()
+
+
+def show_main_page():
+    image = Image.open('data/plane_zakat.jpg')
+
+    st.set_page_config(
+        layout='wide',
+        page_title='Удовлетворенность полетом',
+        page_icon=image,
+        initial_sidebar_state='auto',
+
+    )
+
+    st.write(
+        """
+        # Классификация впечатлений клиентов авиакомпании
+        Определяем, кому из пассажиров понравился полёт, а кому нет.
+        """
+    )
+
+    st.image(image)
+
+
+def input_to_df(gender,ticket,loyality,age,distance,trip_type,
+                dep_delay, arr_delay,
+                booking, baggage, seat, wifi, inf_service, onl_board, gate,
+                clean, food, onb_service, ch_service, dep_arr_time, leg_room, inf_ent):
+    
+    rule = {
+            'Женский': 'Female',
+            'Мужской': 'Male',
+            'Личная': 'Personal Travel',
+            'По работе': 'Business travel',
+            'Да': 'Loyal Customer',
+            'Нет': 'disloyal Customer',
+            'Эко': 'Eco',
+            'Эко плюс': 'Eco plus',
+            'Бизнес': 'Business',
+            }
+
+    data = {
+            'Class': rule[ticket],
+            'Gender': rule[gender],
+            'Age': age,
+            'Customer Type': rule[loyality],
+            'Type of Travel': rule[trip_type],       
+            'Flight Distance': distance,
+            'Departure Delay in Minutes': dep_delay,
+            'Arrival Delay in Minutes': arr_delay,
+            'Inflight wifi service': wifi,
+            'Departure/Arrival time convenient': dep_arr_time,
+            'Ease of Online booking': booking,
+            'Gate location': gate,
+            'Food and drink': food,
+            'Online boarding': onl_board,
+            'Seat comfort': seat,
+            'Inflight entertainment': inf_ent,
+            'On-board service': onb_service,
+            'Leg room service': leg_room,
+            'Baggage handling': baggage,
+            'Checkin service': ch_service,
+            'Inflight service': inf_service,
+            'Cleanliness': clean,
+            }
+
+
+    df = pd.DataFrame(data, index=[0])
+    #st.write(df.head(5)) #debug
+    df=encoding_and_scaling(df, app=True)
+    #st.write(df.head(5)) #debug
+    return df
+
+
+
+def render_main_page():
+
+    st.write('Введите информацию о себе')
+    col1, col2 = st.columns(2)
+
+    with col1:
+        gender = st.selectbox('Ваш пол', ['Мужской','Женский'])
+        ticket = st.selectbox('Класс билета', ['Бизнес', 'Эко', 'Эко плюс'])
+        loyality = st.selectbox('Есть ли у вас карта лояльности', ['Да', 'Нет'])
+
+    with col2:
+        age = st.slider('Ваш возраст', min_value=5, max_value=80)
+        distance = st.slider('Длина перелёта', min_value=100, max_value=4000)
+        trip_type = st.selectbox('Цель поездки', ['Личная', 'По работе'])
+    
+
+    st.divider()
+    st.write('Были ли задержки в расписании?')
+    col1, col2 = st.columns(2)
+
+    with col1:
+        dep_delay = st.slider('Задержка вылета, мин', min_value=0, max_value=180)
+    with col2:
+        arr_delay = st.slider('Задержка прилёта, мин', min_value=0, max_value=180)
+
+    st.divider()
+
+
+    st.write('Заполните анкету')
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        booking = st.radio('Онлайн бронирование', [0, 1, 2, 3, 4, 5])
+        baggage = st.radio('Обращение с багажом', [0, 1, 2, 3, 4, 5])
+        seat = st.radio('Удобство кресла', [0, 1, 2, 3, 4, 5])
+        wifi = st.radio('Wi-fi на борту', [0, 1, 2, 3, 4, 5])
+        inf_service = st.radio('Обслуживание на борту', [0, 1, 2, 3, 4, 5])      
+
+    with col2:
+        onl_board = st.radio('Онлайн регистрация', [0, 1, 2, 3, 4, 5])
+        gate = st.radio('Расположение выхода на посадку', [0, 1, 2, 3, 4, 5])
+        clean = st.radio('Чистота на борту', [0, 1, 2, 3, 4, 5])
+        food = st.radio('Еда и напитки на борту', [0, 1, 2, 3, 4, 5])
+        onb_service = st.radio('Обслуживание на посадке', [0, 1, 2, 3, 4, 5])      
+
+    with col3:
+        ch_service = st.radio('Регистрация на рейс', [0, 1, 2, 3, 4, 5])
+        dep_arr_time = st.radio('Время вылета и прилета', [0, 1, 2, 3, 4, 5])
+        leg_room = st.radio('Место в ногах', [0, 1, 2, 3, 4, 5])
+        inf_ent = st.radio('Развлечения на борту', [0, 1, 2, 3, 4, 5])
+    
+    
+    col1,col2,col3 = st.columns(3)
+    if col2.button('Рассчитать'):
+
+        user_df = input_to_df(gender,ticket,loyality,age,distance,trip_type,
+                dep_delay, arr_delay,
+                booking, baggage, seat, wifi, inf_service, onl_board, gate,
+                clean, food, onb_service, ch_service, dep_arr_time, leg_room, inf_ent)
+
+        with st.spinner('Рассчитываем...'):
+            time.sleep(1)
+            write_predict(user_df)
+
+
+def write_predict(df: pd.DataFrame):
+
+    pred, proba = predict_on_input(df)
+
+    if pred == 1:
+        st.success('Ура! Пассажир доволен!')
+        with st.expander('Подробнее'):
+            st.write(f'Вероятность этого: **`{round(max(proba[0]), 3)}`**')
+    elif pred == 0:
+        st.error('О нет! Пассажир не доволен...')
+        with st.expander('Подробнее'):
+            st.write(f'Вероятность этого: **`{round(max(proba[0]), 3)}`**')  
+    else:
+        st.error('Ошибка, что-то пошло не так...')
+
+    #st.write(f'Дебаг пред {pred}') #debug
+    #st.write(f'Дебаг проба {proba}') #debug
+
+
+
+if __name__ == "__main__":
+
+    process_main_page()
---
+++ model.py
@@ -0,0 +1,184 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.metrics import recall_score, accuracy_score, precision_score
+
+import pickle
+
+#turning off SettingWithCopyWarning: A value is trying to be set on a copy of a slice from a DataFrame
+pd.options.mode.chained_assignment = None 
+
+#open data from given path
+def open_data(path):
+
+    df = pd.read_csv(path)
+
+    return df
+
+#preprocessing numeric (int, float) data
+def preprocessing_numeric_data(df: pd.DataFrame):
+
+    df['Age'] = np.where(df['Age'] > 80, np.NaN, df['Age'])
+    df['Age'] = np.where(df['Age'] == 0, np.NaN, df['Age'])
+
+    df['Flight Distance'] = np.where(df['Flight Distance'] > 4000, np.NaN, df['Flight Distance'])
+    df['Flight Distance'] = np.where(df['Flight Distance'] == 0, np.NaN, df['Flight Distance'])
+
+    departure_99 = np.nanpercentile(df['Departure Delay in Minutes'], 99)
+    df['Departure Delay in Minutes'] = np.where(df['Departure Delay in Minutes'] > departure_99, np.NaN, df['Departure Delay in Minutes'])
+
+    arrival_99 = np.nanpercentile(df['Arrival Delay in Minutes'], 99)
+    df['Arrival Delay in Minutes'] = np.where(df['Arrival Delay in Minutes'] > arrival_99, np.NaN, df['Arrival Delay in Minutes'])
+    
+
+    return df
+
+#preprocessing estimated (from 0 to 5) data
+def preprocessing_estimated_data(df: pd.DataFrame):
+
+    cathegorial = list(df.select_dtypes(include=['float64']))[4:]
+    for col in cathegorial:
+        df[col] = np.where(df[col] > 5, np.NaN, df[col])
+
+    return df
+
+#preprocessing categorical (male-female) data
+def preprocessing_categorical_data(df: pd.DataFrame):
+
+    df = df[df['satisfaction'] != '-']
+
+    return df
+
+#preprocessing data which is NaN
+def processing_missing_data(df: pd.DataFrame):
+
+    nulls = pd.DataFrame(df.isna().sum(), columns=['NaN count'])
+    has_nulls = nulls[nulls['NaN count'] > 0].index
+    for col in has_nulls:
+        if df[col].dtype == 'int64' or df[col].dtype == 'float64':
+            df[col].fillna(df[col].mean(), inplace=True)
+    
+    object_cols = list(df.select_dtypes(include=['object']))
+    df.dropna(subset = object_cols, inplace=True)
+
+    
+    return df
+
+#encode categorial data and scale all data for fitting the model
+def encoding_and_scaling(df: pd.DataFrame, app=False):
+
+    df['Gender'] = df['Gender'].map({'Male' : 1, 'Female' : 0})
+    df['Customer Type'] = df['Customer Type'].map({'Loyal Customer' : 1, 'disloyal Customer' : 0})
+    df['Type of Travel'] = df['Type of Travel'].map({'Business travel' : 1, 'Personal Travel' : 0})
+    
+    categorical = ['Class']
+    numeric_features = [col for col in df.columns if col not in categorical and col not in ['satisfaction', 'id']]
+
+    if not app:
+        df['satisfaction'] = df['satisfaction'].map({'satisfied' : 1, 'neutral or dissatisfied' : 0})
+
+
+        column_transformer = ColumnTransformer([
+            ('ohe', OneHotEncoder(drop='first', handle_unknown="ignore"), categorical),
+            ('scaling', MinMaxScaler(), numeric_features)
+            ])
+        
+        df= column_transformer.fit_transform(df)
+
+        lst = list(column_transformer.transformers_[0][1].get_feature_names_out())
+        lst.extend(numeric_features)
+
+        df = pd.DataFrame(df, columns=lst)
+
+    else:
+        column_transformer = pickle.load(open('data/column_transformer.pkl', 'rb'))
+        df = column_transformer.transform(df)
+        
+        lst = list(column_transformer.transformers_[0][1].get_feature_names_out())
+        lst.extend(numeric_features)
+
+        df = pd.DataFrame(df, columns=lst) 
+
+    return df
+
+
+#split data to train and test
+def split_data(df: pd.DataFrame, test=False):
+
+    X = df.drop(['id', 'satisfaction'], axis=1)
+    y = df['satisfaction'] #target
+
+    if test:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+        return X_train, X_test, y_train, y_test
+    else:
+        return X, y
+
+#prepare data for training
+def prepare_data(df: pd.DataFrame, test=False):
+
+    df = preprocessing_numeric_data(df)
+    df = preprocessing_estimated_data(df)
+    df = preprocessing_categorical_data(df)
+    df = processing_missing_data(df)
+    df = encoding_and_scaling(df)
+
+    if test:
+        X_train, X_test, y_train, y_test = split_data(df)
+        return X_train, X_test, y_train, y_test
+    else:
+        X_train, y_train = split_data(df)
+        return X_train, y_train
+
+#fits logistic regression model
+def fit_and_save(df: pd.DataFrame, path="data/model.pickle", test=False):
+
+    if test:
+        X_train, X_test, y_train, y_test = prepare_data(df)
+    else:
+        X_train, y_train = prepare_data(df)
+
+    model = LogisticRegression(max_iter=500, random_state=42)
+    model.fit(X_train, y_train)
+
+    if test:
+        preds = model.predict(X_test)
+        print(f'Accuracy: {round(accuracy_score(y_test, preds), 3)}')
+        print(f'Precision: {round(precision_score(y_test, preds), 3)}')
+        print(f'Recall: {round(recall_score(y_test, preds), 3)}')
+
+    with open(path, 'wb') as f:
+        pickle.dump(model, f)
+        print(f"Model was saved to {path}")
+        
+
+
+#load preload model
+def load_model(path="data/model.pickle"):
+    with open(path, 'rb') as f:
+        model = pickle.load(f)
+    return model
+
+
+#make prediction on input
+def predict_on_input(df: pd.DataFrame):
+
+    model=load_model()
+
+    prediction = model.predict(df)[0]
+
+    prediction_proba = model.predict_proba(df)
+
+    return prediction, prediction_proba
+
+if __name__ == "__main__":
+
+    df = open_data(path = "data/clients.csv")
+
+    fit_and_save(df)
